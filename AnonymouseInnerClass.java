af4349340b6fce2cf0b04a439c93b11d9a707683
MKalpana407 <99788289+MKalpana407@users.noreply.github.com>
2022-02-27 13:11:36 +0530

    Add files via upload

---
+++ AnonymouseInnerClass.java
@@ -0,0 +1,21 @@
+package abc;
+abstract class AnonymousInnerClass {
+	   public abstract void display();
+	}
+
+
+	public class AnonymouseInnerClass {
+
+	public static void main(String[] args) {
+	AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
+
+
+
