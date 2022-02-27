bd4509291973cbc1d80fa9e411724c9aeaf1006e
MKalpana407 <99788289+MKalpana407@users.noreply.github.com>
2022-02-27 18:06:47 +0530

    Add files via upload

---
+++ DaimendProblem.java
@@ -0,0 +1,30 @@
+package abc;
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class DaimendProblem implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+    	DaimendProblem ob = new DaimendProblem(); 
+        ob.show(); 
+    } 
+}
+
+
