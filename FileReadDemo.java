4929913003fc15e37145d7a653588c75f6a5e723
MKalpana407 <99788289+MKalpana407@users.noreply.github.com>
2022-02-27 17:19:27 +0530

    Add files via upload

---
+++ FileReadDemo.java
@@ -0,0 +1,26 @@
+package abc;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char[] data = new char[100];
+try
+{
+FileReader input= new FileReader("data1.txt");
+input.read(data);
+System.out.println(data);
+input.close();
+
+	}
+catch (IOException ex)
+{
+	System.out.println("File read Error");
+}
+
+}
+}
