f74b04da4a84b5fc1a5dc175d32fb10a9d54eb15
MKalpana407 <99788289+MKalpana407@users.noreply.github.com>
2022-02-27 13:03:48 +0530

    Add files via upload

---
+++ ProtectedAccessSpecifier22.java
@@ -0,0 +1,12 @@
+package p2;
+import p1.*;
+
+public class ProtectedAccessSpecifier22  extends ProtectedAccessSpecifier {
+
+	public static void main(String[] args) {
+		ProtectedAccessSpecifier22 obj = new ProtectedAccessSpecifier22();   
+	       obj.display();  
+	}
+
+}
+
