14fbaa7c1d572d379ed88f589a5bcb7fa0c699a6
MKalpana407 <99788289+MKalpana407@users.noreply.github.com>
2022-02-27 13:05:10 +0530

    Add files via upload

---
+++ PublicAccessSepicifier22.java
@@ -0,0 +1,16 @@
+package pack2;
+import pack1.*;
+
+public class PublicAccessSepicifier22 {
+
+	public static void main(String[] args) {
+		
+		PublicAccessSepicifier  obj = new PublicAccessSepicifier(); 
+        obj.display();  
+		
+	}
+
+	
+}
+
+
